493cfbd427f7e4dfc1acd9904c8d17d1a802989b
sunnwave <ssun20886@gmail.com>
2026-04-04 20:16:43 +0900

    🚚[Rename] import 문 수정

--- pages/records/new/index.tsx
+++ pages/records/new/index.tsx
@@ -1,4 +1,4 @@
-import RecordWriteScreen from "@/components/features/record-write/RecordWriteScreen";
+import RecordWriteScreen from "@/components/features/record-write/ui/RecordWriteScreen";
 
 export default function NewRecordPage() {
   return <RecordWriteScreen />;
