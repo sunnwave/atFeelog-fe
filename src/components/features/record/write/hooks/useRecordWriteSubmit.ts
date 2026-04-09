b921c48abfec641cf410fd7878b803c30c5b6de5
sunnwave <ssun20886@gmail.com>
2026-04-09 20:22:09 +0900

    ♻️[Refactor] useRecordWriteSubmit.ts, submitRecordWrite.ts 통합

--- src/components/features/record/write/hooks/useRecordWriteSubmit.ts
+++ src/components/features/record/write/hooks/useRecordWriteSubmit.ts
@@ -4,8 +4,8 @@ import { useRouter } from "next/router";
 import { useRecoilValue } from "recoil";
 import { useToast } from "@/components/commons/toast/ToastProvider";
 import { RecordWriteFormValues } from "../../model";
-import { submitRecordWrite } from "../lib/submitRecordWrite";
 import { useCreateRecord } from "./mutations/useCreateRecord";
+import { mapRecordWriteToCreateBoardInput } from "../../editor";
 
 export default function useRecordWriteSubmit() {
   const me = useRecoilValue(loggedInUserState);
@@ -18,15 +18,18 @@ export default function useRecordWriteSubmit() {
   const isBusy = isUploading || loading;
 
   const onSubmitValid = async (values: RecordWriteFormValues) => {
+    const uploadedUrls = await uploadImages(values.imageFiles);
+
+    const args = mapRecordWriteToCreateBoardInput({
+      values: { ...values, images: uploadedUrls },
+      writer: me?.name || "익명",
+      password: me?._id || "1234",
+    });
+
     if (isBusy) return;
 
     try {
-      const id = await submitRecordWrite({
-        values,
-        me,
-        uploadImages,
-        createRecord: onCreateRecord,
-      });
+      const id = await onCreateRecord(args);
       success("필로그를 기록했어요📖✨");
       await router.push(`/records/${id}`);
     } catch (e) {
