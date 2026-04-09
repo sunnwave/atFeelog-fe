e3b67f97c1624fcaef2ca17d8654b1711395742e
sunnwave <ssun20886@gmail.com>
2026-04-09 20:20:37 +0900

    ✨[Feat] RecordUpdate 기능 구현(useUpdateRecord.ts, useRecordUpdateSubmit.ts)

---
+++ src/components/features/record/update/hooks/useRecordUpdateSubmit.ts
@@ -0,0 +1,46 @@
+import { useUploadImages } from "@/shared/hooks/image/useUploadImages";
+import { loggedInUserState } from "@/shared/stores";
+import { useRouter } from "next/router";
+import { useRecoilValue } from "recoil";
+import { useUpdateRecord } from "./useUpdateRecord";
+import { useToast } from "@/components/commons/toast/ToastProvider";
+import { RecordWriteFormValues } from "../../model";
+import { mapRecordUpdateToUpdateBoardInput } from "../../editor";
+
+export default function useRecordUpdateSubmit() {
+  const me = useRecoilValue(loggedInUserState);
+  const router = useRouter();
+
+  const { uploadImages, isUploading } = useUploadImages();
+  const { onUpdateRecord, loading } = useUpdateRecord();
+  const { error, success } = useToast();
+
+  const isBusy = isUploading || loading;
+
+  const onSubmitValid = async (
+    values: RecordWriteFormValues,
+    boardId: string
+  ) => {
+    const uploadedUrls = await uploadImages(values.imageFiles);
+
+    const args = mapRecordUpdateToUpdateBoardInput({
+      values: { ...values, images: uploadedUrls },
+      password: me?._id || "1234",
+      boardId,
+    });
+
+    if (isBusy) return;
+
+    try {
+      const id = await onUpdateRecord({ ...args });
+      success("필로그를 수정했어요📖✨");
+      await router.push(`/records/${id}`);
+    } catch (e) {
+      const message =
+        e instanceof Error ? e.message : "필로그 수정에 실패했어요😢";
+      error(message);
+    }
+  };
+
+  return { onSubmitValid, isBusy };
+}
