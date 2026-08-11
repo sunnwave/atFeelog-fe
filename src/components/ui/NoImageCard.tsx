5d2609334d9aff6bafa0cb23f7f161f790b53c0a
sun <ssun20886@gmail.com>
2026-08-11 11:34:42 +0900

    🚚[Rename] NoImageCard 공통 컴포넌트로 이동

--- src/components/ui/NoImageCard.tsx
+++ src/components/ui/NoImageCard.tsx
@@ -1,10 +1,6 @@
 import { ReactNode } from "react";
 
-export default function NoImagePosterCard({
-  children,
-}: {
-  children: ReactNode;
-}) {
+export default function NoImageCard({ children }: { children?: ReactNode }) {
   return (
     <div
       className="w-full h-full flex flex-col justify-between p-2 pb-4 @card-xs:p-3 @card-xs:pb-5 @card-sm:p-4 @card-sm:pb-6 @card-md:p-5 @card-md:pb-7 @card-lg:p-6 @card-lg:pb-8 bg-white group-hover:bg-foreground transition-all duration-300"
