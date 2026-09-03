21c3a7a4cd35321cae2042cdbd7be4cb037a7756
sun <ssun20886@gmail.com>
2026-09-03 13:02:45 +0900

    🚚[Rename] user/hooks/query, mutation으로 폴더 구분. barrel export 적용

---
+++ src/components/features/user/hooks/query/index.ts
@@ -0,0 +1,8 @@
+export { useFetchBoardsByUser } from "./useFetchBoardsByUser";
+export { useFetchBoardsCountByUser } from "./useFetchBoardsCountByUser";
+export { useFetchBoardsLikeByUser } from "./useFetchBoardsLikeByUser";
+export { useFetchBoardsLikeCountByUser } from "./useFetchBoardsLikeCountByUser";
+export { useFetchCountOfFollowers } from "./useFetchCountOfFollowers";
+export { useFetchCountOfFollowing } from "./useFetchCountOfFollowing";
+export { useFetchFollowers } from "./useFetchFollowers";
+export { useFetchFollowing } from "./useFetchFollowing";
