18c069e2a29f306399924f24b523f7c4cf9a2d9c
sun <ssun20886@gmail.com>
2026-09-09 21:34:30 +0900

    ✨[Feat] kakao.ts MSW handler 구현 및 스토리북 적용

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -1,3 +1,4 @@
 import { kopisHandlers } from "./kopis";
+import { kakaoHandlers } from "./kakao";
 
-export const handlers = [...kopisHandlers];
+export const handlers = [...kopisHandlers, ...kakaoHandlers];
