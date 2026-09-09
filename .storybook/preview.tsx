3bb99a4359e0df5d8c51c43cc7e2825838ca34d9
sun <ssun20886@gmail.com>
2026-09-09 20:23:57 +0900

    📦[Chore] 스토리북 msw 환경설정(msw 초기화, 핸들러 등록,addon 추가)

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,12 @@
 import type { Preview } from "@storybook/nextjs-vite";
 import { MockedProvider } from "@apollo/client/testing";
+import { initialize, mswDecorator } from "msw-storybook-addon";
 import "../src/styles/globals.css";
 import { ToastProvider } from "@/components/commons/toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { handlers } from "../src/mocks/handlers";
+
+initialize({ onUnhandledRequest: "bypass" }, handlers);
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,6 +16,7 @@ const queryClient = new QueryClient({
 
 const preview: Preview = {
   decorators: [
+    mswDecorator,
     (Story) => (
       <QueryClientProvider client={queryClient}>
         <MockedProvider mocks={[]} addTypename={false}>
